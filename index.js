53d61ff9b58ceb6d1307c0564e95bfa5121373f3
Lorena <lorena.giddens@outlook.com>
2018-04-16 05:02:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,26 +21,13 @@ function deepestChild() {
 
 // Define a function increaseRankBy(n) that increases the ranks in all of the .ranked-lists by n
 
-/*function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('ul.ranked-list li');
-  const firstList = rankedLists[0];
-  const secondList = rankedLists[1];
-  
-  let children = firstList.children;
-  let start = 1;
-  
-  for (let i = 0, l = children.length; i < l; i++) {
-    parseInt(children[i].innerHTML) + n;
-  }
-  
-  children = secondList.children;
-  start = 12;
-
-  for (let i = 0, l = children.length; i < l; i++) {
-    parseInt(children[i].innerHTML) + start + i + n;
-  }
-  return rankedLists;
-}*/
+function increaseRankBy(n) {
+var rankedLists = document.querySelectorAll('.ranked-list li')
+  for (var i=0; i<rankedLists.length; i++) {
++    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML)+n 
++  }
++  return rankedLists
++}
 
 function increaseRankBy(n) {
   const children = document.querySelectorAll('ul.ranked-list li');
